8355b8b8e91ee173fb4296e03c3d61db59813ff7
Anurag Khandelwal <anuragk@berkeley.edu>
2018-08-02 23:02:19 -0700

    Updated benchmark_handler

--- src/benchmark_handler.py
+++ src/benchmark_handler.py
@@ -4,7 +4,6 @@ import os
 import shutil
 import socket
 import subprocess
-import sys
 
 import boto3
 from six.moves import configparser
@@ -16,6 +15,24 @@ BIN_DIR = '/tmp/bin'
 EXECUTABLE = 'storage_bench'
 
 
+class Logger(object):
+    def __init__(self, f):
+        self.f = f
+
+    def info(self, msg):
+        self._log('INFO', msg)
+
+    def warn(self, msg):
+        self._log('WARN', msg)
+
+    def error(self, msg):
+        self._log('ERROR', msg)
+
+    def _log(self, msg_type, msg):
+        self.f.write('{} {}'.format(msg_type, msg))
+        self.f.flush()
+
+
 def _init_bin(executable_name):
     if not os.path.exists(BIN_DIR):
         os.makedirs(BIN_DIR)
@@ -25,39 +42,34 @@ def _init_bin(executable_name):
     os.chmod(new_file, 0o775)
 
 
-def _copy_results(result):
-    print('Copying results @ {}'.format(result))
+def _copy_results(logger, result):
+    logger.info('Copying results @ {}'.format(result))
     bucket = boto3.resource('s3').Bucket('bench-results')
     with open(result, 'rb') as data:
         bucket.put_object(Key=result, Body=data)
 
 
-def _run_benchmark(system, conf, out, bench):
+def _run_benchmark(logger, system, conf, out, bench):
     _init_bin(EXECUTABLE)
     cmdline = [os.path.join(BIN_DIR, EXECUTABLE), system, conf, out, bench]
-    print('Running benchmark, cmd: {}'.format(cmdline))
+    logger.info('Running benchmark, cmd: {}'.format(cmdline))
     subprocess.check_call(cmdline, shell=False, stderr=subprocess.STDOUT)
 
 
-def _create_ini(system, conf, out):
+def _create_ini(logger, system, conf, out):
     config = configparser.ConfigParser()
     config.add_section(system)
     for key in conf.keys():
         config.set(system, key, conf[key])
     with open(out, 'w') as f:
         conf.write(f)
-    print('Created configuration file {}'.format(out))
+    logger.info('Created configuration file {}'.format(out))
 
 
-def _redirect_output(host, port):
-    print('Connecting to {}:{}'.format(host, port))
+def _connect_logger(host, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((host, port))
-    print('Connection successful; replacing stdout/stderr...')
-    file = sock.makefile('w')
-    sys.stdout = file
-    sys.stderr = file
-    print('Done.')
+    return Logger(sock.makefile('w'))
 
 
 def benchmark_handler(event, context):
@@ -66,14 +78,14 @@ def benchmark_handler(event, context):
     host = event['host']
     port = event['port']
 
-    print('System: {}, conf: {}, log.host: {}, log.port: {}'.format(system, conf, host, port))
-
     try:
-        _redirect_output(host, port)
+        logger = _connect_logger(host, port)
     except Exception as e:
         print('Exception: {}'.format(e))
         raise
 
+    logger.info('System: {}, conf: {}, logger.host: {}, logger.port: {}'.format(system, conf, host, port))
+
     prefix = os.path.join('/tmp', system)
-    _create_ini(system, conf, prefix + '.conf')
-    _run_benchmark(system, prefix + '.conf', prefix, 'sweep')
+    _create_ini(logger, system, conf, prefix + '.conf')
+    _run_benchmark(logger, system, prefix + '.conf', prefix, 'sweep')
