d653be6e57dfdfb11d377a28974066ac39f6b019
Anurag Khandelwal <anuragk@berkeley.edu>
2018-08-03 01:41:11 -0700

    Updated benchmark

--- src/benchmark_handler.py
+++ src/benchmark_handler.py
@@ -4,11 +4,121 @@ import os
 import shutil
 import socket
 import subprocess
+import sys
 
 import boto3
 from six.moves import configparser
 
-from src.compat import b
+if sys.version_info[0] < 3:
+    try:
+        from cStringIO import StringIO as BytesIO
+    except ImportError:
+        from StringIO import StringIO as BytesIO
+
+
+    def safe_unicode(obj, *args):
+        """ return the unicode representation of obj """
+        try:
+            return unicode(obj, *args)
+        except UnicodeDecodeError:
+            # obj is byte string
+            ascii_text = str(obj).encode('string_escape')
+            return unicode(ascii_text)
+
+
+    def iteritems(x):
+        return x.iteritems()
+
+
+    def iterkeys(x):
+        return x.iterkeys()
+
+
+    def itervalues(x):
+        return x.itervalues()
+
+
+    def nativestr(x):
+        return x if isinstance(x, str) else x.encode('utf-8', 'replace')
+
+
+    def u(x):
+        return x.decode()
+
+
+    def b(x):
+        return x
+
+
+    def next(x):
+        return x.next()
+
+
+    def byte_to_chr(x):
+        return x
+
+
+    def char_to_byte(x):
+        return x
+
+
+    def bytes_to_str(x):
+        return x
+
+
+    unichr = unichr
+    xrange = xrange
+    basestring = basestring
+    unicode = unicode
+    bytes = str
+    long = long
+else:
+    def iteritems(x):
+        return iter(x.items())
+
+
+    def iterkeys(x):
+        return iter(x.keys())
+
+
+    def itervalues(x):
+        return iter(x.values())
+
+
+    def byte_to_chr(x):
+        return chr(x)
+
+
+    def char_to_byte(x):
+        return ord(x)
+
+
+    def nativestr(x):
+        return x if isinstance(x, str) else x.decode('utf-8', 'replace')
+
+
+    def bytes_to_str(x):
+        return x.decode()
+
+
+    def u(x):
+        return x
+
+
+    def b(x):
+        return x.encode('latin-1') if not isinstance(x, bytes) else x
+
+
+    next = next
+    unichr = chr
+    imap = map
+    izip = zip
+    xrange = range
+    basestring = str
+    unicode = str
+    safe_unicode = str
+    bytes = bytes
+    long = int
 
 
 class NetworkFile(object):
