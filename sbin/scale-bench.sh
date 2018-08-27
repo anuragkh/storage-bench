e2c1017803f2f695c4b01390728cd11c03adb1e8
Anurag Khandelwal <anuragk@berkeley.edu>
2018-08-27 19:07:48 +0000

    Updated scripts

--- sbin/scale-bench.sh
+++ sbin/scale-bench.sh
@@ -5,12 +5,12 @@ sys=$1
 bin="/tmp/build"
 conf="$sbin/../conf/storage_bench.conf"
 bench="$sbin/../lambda_benchmark.py"
-mode="scale:read:40:20:5"
+mode="scale:read:200:20:5"
 size="1024"
-nops="1000000"
+nops="1024000"
 dist="zipf"
 log="/tmp/${sys}-${size}-${nops}-${dist}"
 
 python $bench --conf $conf --invoke-local --bin-path $bin --system $sys\
-  --bench-mode $mode --object-size $size --num-ops $nops --distribution $dist\
-  1>${log}.stdout 2>${log}.stderr
+  --mode $mode --obj-size $size --num-ops $nops --dist $dist 1>${log}.stdout\
+  2>${log}.stderr
