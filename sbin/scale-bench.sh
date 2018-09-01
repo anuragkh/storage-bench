511b4f692abf72a4c52c55acfc3af9ab5dd3489c
Anurag Khandelwal <anuragk@berkeley.edu>
2018-09-01 21:23:58 +0000

    Updated benchmark

--- sbin/scale-bench.sh
+++ sbin/scale-bench.sh
@@ -7,9 +7,10 @@ conf="$sbin/../conf/storage_bench.conf"
 bench="$sbin/../lambda_benchmark.py"
 mode="scale:read:200:20:5"
 size="1024"
-nops="1024000"
+nops="1000000"
 dist="zipf"
 log="/tmp/${sys}-${size}-${nops}-${dist}"
 
-python $bench --conf $conf --invoke-local --bin-path /tmp/build --system $sys --mode $mode\
-  --obj-size $size --num-ops $nops --dist $dist 1>${log}.stdout 2>${log}.stderr
+stdbuf -o0 python $bench --quiet --conf $conf --invoke-local --bin-path $bin\
+  --system $sys --mode $mode --obj-size $size --num-ops $nops --dist $dist\
+  1>${log}.stdout 2>${log}.stderr
